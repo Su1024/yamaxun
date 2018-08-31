1b675ef87f500f1d10fabd9ce2ab06c70a945c8d
SuGM <18888158689@163.com>
2018-09-01 01:06:57 +0800

    获取 页面资源

--- yamaxun/spiders/book.py
+++ yamaxun/spiders/book.py
@@ -6,15 +6,18 @@ from scrapy.spiders import CrawlSpider, Rule
 
 class BookSpider(CrawlSpider):
     name = 'book'
-    allowed_domains = ['www.amazon.cn']
-    start_urls = ['http://www.amazon.cn/']
+    allowed_domains = ['www.amazon.cn','www.amazon.cn']
+    start_urls = ['https://www.amazon.cn/图书/b?node=658390051']
 
     rules = (
-        Rule(LinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
+        Rule(LinkExtractor(allow=r'/s/ref=lp_'), callback='parse_item', follow=True),
     )
 
     def parse_item(self, response):
         i = {}
+        a_list = response.xpath('//div[@class="ch-tabwidget-asintitle"]/a/text()').extract()
+        print(a_list)
+
         #i['domain_id'] = response.xpath('//input[@id="sid"]/@value').extract()
         #i['name'] = response.xpath('//div[@id="name"]').extract()
         #i['description'] = response.xpath('//div[@id="description"]').extract()
